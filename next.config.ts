78c05340179752efe72bf482366868c5aae9a40b
waleednisar687-art <waleednisar687@gmail.com>
2025-12-17 15:15:55 +0500

    nexr

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/webmail',
+        destination: 'https://webmail.oasisfusionservices.com',
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
